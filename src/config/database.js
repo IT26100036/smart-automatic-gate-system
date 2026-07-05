81f20a05d8ce8b56aee328320aa5a15a6bc94a46
IT26100036 <it26100036@my.sliit.lk>
2026-07-05 16:01:30 +0530

    feat: add MONGO_DB_NAME to environment configuration for database connection

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
+      dbName: process.env.MONGO_DB_NAME || "smartgate_db",
       serverSelectionTimeoutMS: 10000,
       socketTimeoutMS: 45000,
     });
