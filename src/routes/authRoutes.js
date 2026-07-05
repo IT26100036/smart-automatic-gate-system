90190552401f87d722368562b32fec201b9fd983
IT26100036 <it26100036@my.sliit.lk>
2026-07-05 16:23:33 +0530

    feat: remove adminOnly middleware from user registration route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { registerUser, loginUser, updateProfile } = require("../controllers/authController");
 const { protect, adminOnly } = require("../middleware/authMiddleware");
 
-router.post("/register", protect, adminOnly, registerUser);
+router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.put("/profile", protect, updateProfile);
 
